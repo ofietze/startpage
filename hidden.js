dd5ffd65ae53b12d30fdbd042edea84844b5316b
4T2 <f.oliver@live.de>
2018-06-07 22:56:37 +0200

    added toggle for hidden elements

--- hidden.js
+++ hidden.js
@@ -1,34 +1,41 @@
 var totalSeconds = 0;
 var t;
-var toggle = false;
+var toggleTime = false;
+var toggleS = false;
 
-function show() {
-  var timer = document.getElementById('timerDiv').classList.remove("hidden");
+function toggleShow() {
+  if (toggleS) {
+  var timer = document.getElementById('timerDiv').classList.add("hidden");
+  } else {
+    var timer = document.getElementById('timerDiv').classList.remove("hidden");
+  }
+  toggleS = !toggleS
 }
 
 function toggleTimer(){
-  if (toggle) {
+  if (toggleTime) {
     clearInterval(t)
     document.getElementById('search').placeholder ="00:00"
-    document.getElementById("startStop").value = "I>"
-    toggle = !toggle
+    document.getElementById("startStop").value = "start"
   } else {
     startTimer()
-    document.getElementById("startStop").value = "II"
-    toggle = !toggle
+    document.getElementById("startStop").value = "stop"
   }
+    toggleTime = !toggleTime
 }
 
 function startTimer() {
   var minutes = parseInt(document.getElementById("inputTimeMin").value);
   var seconds = parseInt(document.getElementById("inputTimeSec").value);
-  console.log("min: " + minutes);
-  console.log("sec: " + seconds);
+
+  // check for empty input and set corresponding value to 0
+  minutes = minutes !== minutes ? minutes = 0 : minutes = minutes
+  seconds = seconds !== seconds ? seconds = 0 : seconds = seconds
+
   totalSeconds = minutes * 60 + seconds;
 
   formatSeconds(totalSeconds);
   t = setInterval(updateTimer, 1000)
-
 }
 
 function updateTimer(){
@@ -36,14 +43,14 @@ function updateTimer(){
     alert("Timer done!");
     clearInterval(t);
     document.getElementById('search').placeholder = "fin"
-  }else{
+
+  } else{
     totalSeconds--;
     formatSeconds(totalSeconds);
   }
 }
 
 function formatSeconds(){
-  console.log(totalSeconds);
   document.getElementById('search').placeholder =
   checkTime(Math.floor(totalSeconds/60) % 60) +":"
   + checkTime(totalSeconds % 60);
